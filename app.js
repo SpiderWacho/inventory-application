9ed315cd799cad9c0117347923a8a09eef0f964b
SpiderWacho <iamgaas@gmail.com>
2023-07-24 18:38:14 -0300

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const RateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 // Import the mongoose module
 const mongoose = require("mongoose");
+
+const compression = require("compression");
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog')
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 var app = express();
 
@@ -22,6 +34,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(limiter);
+app.use(compression()); // Compress all routes
+
+
+// Set up rate limiter: maximum of twenty requests per minute
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -29,7 +53,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.set("strictQuery", false);
 // Define the database URL to connect to.
-const mongoDB = "mongodb+srv://spider:kVunRROTmXqW4vQL@cluster0.hktvh3o.mongodb.net/game_store?retryWrites=true&w=majority";
+const dev_db_url =
+  "mongodb+srv://spider:kVunRROTmXqW4vQL@cluster0.hktvh3o.mongodb.net/game_store?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGO_URL || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
